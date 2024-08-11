9df3bb746685772d252fcef910fa93103d914482
guolunwei <lunwei.guo@hotmail.com>
2024-08-11 15:56:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ GITHUB_PRIVATE_KEY = os.environ.get("github_private_key", "")
 GITEE_TOKEN = os.environ.get("gitee_token", "your_gitee_access_token_here")
 GITEE_PRIVATE_KEY = os.environ.get("gitee_private_key", "")
 
+SYNC_DIRECTION = os.environ.get("sync_direction", "github2gitee")
+
 REPO_LIST = ["ansible-prometheus"]
 EXCLUDED_REPO_LIST = [
     "get_info",
