84b7b66b121e60b1003ff86eed8b3f0339cc5538
guolunwei <lunwei.guo@hotmail.com>
2024-08-11 18:04:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,8 @@ def sync_repo_mirror(base_client, mirror_client):
 
                     local_repo = Repo(tmp_dir)
                     print(dir(local_repo))
-                    # origin = local_repo.remotes.origin
+                    origin = local_repo.remotes.origin
+                    print(origin)
                     # origin.set_url(f'https://{mirror_client.website}.com/{mirror_client.owner}/{repo_name}.git')
                     # print(f"Pushed to url: {repo.remotes.origin.url}")
                     # repo_url = (f'https://{mirror_client.owner}:{mirror_client.access_token}@{mirror_client.website}.com'
