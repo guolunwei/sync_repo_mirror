bc7ac65a42fd45256dd6cb622aa7fd144baaed91
guolunwei <lunwei.guo@hotmail.com>
2024-08-11 15:34:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ GITHUB_PRIVATE_KEY = os.environ.get("github_private_key", "")
 GITEE_TOKEN = os.environ.get("gitee_token", "your_gitee_access_token_here")
 GITEE_PRIVATE_KEY = os.environ.get("gitee_private_key", "")
 
-# REPO_LIST = ["ansible-prometheus"]
-REPO_LIST = ["test"]
+REPO_LIST = ["ansible-prometheus"]
 EXCLUDED_REPO_LIST = [
     "get_info",
     "KeepAliveE5",
