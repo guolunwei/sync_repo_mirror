de5e125d9f998dd313df1a6e8dcdc87af3f50c3d
guolunwei <lunwei.guo@hotmail.com>
2024-08-11 17:51:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,14 +69,14 @@ def sync_repo_mirror(base_client, mirror_client):
 
                     local_repo = Repo(tmp_dir)
                     print(dir(local_repo))
-                    origin = local_repo.remotes.origin
-                    origin.set_url(f'https://{mirror_client.website}.com/{mirror_client.owner}/{repo_name}.git')
-                    print(f"Pushed to url: {repo.remotes.origin.url}")
-                    repo_url = (f'https://{mirror_client.owner}:{mirror_client.access_token}@{mirror_client.website}.com'
-                                f'/{mirror_client.owner}/{repo_name}.git')
-                    print(repo_url)
-                    local_repo.git.push("--all", repo_url)
-                    local_repo.git.push("--tags", repo_url)
+                    # origin = local_repo.remotes.origin
+                    # origin.set_url(f'https://{mirror_client.website}.com/{mirror_client.owner}/{repo_name}.git')
+                    # print(f"Pushed to url: {repo.remotes.origin.url}")
+                    # repo_url = (f'https://{mirror_client.owner}:{mirror_client.access_token}@{mirror_client.website}.com'
+                    #             f'/{mirror_client.owner}/{repo_name}.git')
+                    # print(repo_url)
+                    # local_repo.git.push("--all", repo_url)
+                    # local_repo.git.push("--tags", repo_url)
 
                     # Set repository private property
                     new_repo.edit(private=repo.private)
